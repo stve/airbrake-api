e22e2a8c3adfd6e67b58f2e2e8573ae53d0a2f5c
Steve Agalloco <steve.agalloco@gmail.com>
2010-05-24 21:49:30 -0400

    removed unused hash extension

--- lib/hoptoad-api/core_extensions.rb
+++ lib/hoptoad-api/core_extensions.rb
@@ -3,19 +3,3 @@ class Array
     last.is_a?(::Hash) ? pop : {}
   end
 end
-
-class Hash
-  
-  # Converts all of the keys to strings, optionally formatting key name
-  def rubyify_keys!
-    keys.each{|k|
-      v = delete(k)
-      new_key = k.to_s.underscore
-      self[new_key] = v
-      v.rubyify_keys! if v.is_a?(Hash)
-      v.each{|p| p.rubyify_keys! if p.is_a?(Hash)} if v.is_a?(Array)
-    }
-    self
-  end
-  
-end
