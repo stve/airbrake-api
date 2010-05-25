45b9c3bbaef1366057ab9357decf4e1459199b56
Steve Agalloco <steve.agalloco@gmail.com>
2010-05-24 22:24:17 -0400

    removed extra spaces

--- lib/hoptoad-api.rb
+++ lib/hoptoad-api.rb
@@ -4,29 +4,29 @@ require 'httparty'
 module Hoptoad
   extend self
   attr_accessor :secure
-  
+
   class HoptoadError < StandardError; end
-  
+
   def account=(account)
     @account = account
   end
-  
+
   def account
     "#{protocol}://#{@account}.hoptoadapp.com"
   end
-  
+
   def auth_token=(token)
     @auth_token = token
   end
-  
+
   def auth_token
     @auth_token
   end
-  
+
   def protocol
     secure ? "https" : "http"
   end
-  
+
 end
 
 require 'hoptoad-api/version'
